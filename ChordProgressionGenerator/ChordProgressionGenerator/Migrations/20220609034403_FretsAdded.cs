d774e1cad1a5cd092b2c25ba1e0b4840ec83d04d
Gibb Arko <gibbarko7@gmail.com>
2022-06-10 10:19:13 -0400

    Updated database to include fretboard positioning. Updated code to display chords as tab

---
+++ ChordProgressionGenerator/ChordProgressionGenerator/Migrations/20220609034403_FretsAdded.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ChordProgressionGenerator.Migrations
+{
+    public partial class FretsAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FRETS",
+                table: "Chords",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FRETS",
+                table: "Chords");
+        }
+    }
+}
